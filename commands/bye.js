bc388110ef1fa36cd34fe3119f789993f14cc7b2
joaovsa7 <jvsadesign@gmail.com>
2021-10-20 10:18:25 -0300

    feat: add bye command

---
+++ commands/bye.js
@@ -0,0 +1,4 @@
+module.exports = (message) => {
+  return message.channel.send(`Já está saindo ${message.author.username}?! Tá sem motivação 😨🤔🤔? O que você faz das meia noite às 06h? Zoeira😂😂! Bom descanso, te vejo mais tarde!😴`)
+}
+ 
--- commands/index.js
+++ commands/index.js
@@ -3,11 +3,13 @@ const play = require('./play')
 const queue = require('./queue')
 const skip = require('./skip')
 const hug = require('./hug')
+const bye = require('./bye')
 
 module.exports = {
   '-hello': hello,
   '-play': play,
   '-skip': skip,
   '-queue': queue,
-  '-hug': hug
+  '-hug': hug,
+  '-bye': bye
 }
